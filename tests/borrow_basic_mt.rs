25299a224429ea6cb157599e3ac0dd6e2f17fdbf
Darek Stojaczyk <darek@stojaczyk.pl>
2024-06-01 12:47:46 +0200

    tests: add tests with multiple racing borrowers

--- tests/borrow_basic_mt.rs
+++ tests/borrow_basic_mt.rs
@@ -63,3 +63,56 @@ fn borrow_basic_double_thread() {
     t1.join().unwrap();
     t2.join().unwrap();
 }
+
+#[test]
+fn borrow_basic_multi_borrow() {
+    let mutex = Arc::new(BorrowMutex::<16, TestObject>::new());
+
+    let t1_mutex = mutex.clone();
+    let t1 = std::thread::spawn(|| {
+        futures::executor::block_on(async move {
+            let mut test = TestObject { counter: 1 };
+
+            eprintln!("t1 thread: {:?}", std::thread::current());
+
+            loop {
+                if test.counter >= 20 {
+                    break;
+                }
+                futures::select! {
+                    _ = Delay::new(Duration::from_millis(100)).fuse() => {
+                        if test.counter < 10 {
+                            test.counter += 1;
+                        }
+                        eprintln!("t1: counter: {}", test.counter);
+                    }
+                    _ = t1_mutex.wait_to_lend().fuse() => {
+                        t1_mutex.lend(&mut test).unwrap().await;
+                    }
+                }
+            }
+
+            t1_mutex.terminate().await;
+        })
+    });
+
+    for n in 0..3 {
+        let n = n + 3;
+        let t_mutex = mutex.clone();
+        let t: std::thread::JoinHandle<()> = std::thread::spawn(move || {
+            futures::executor::block_on(async move {
+                eprintln!("t{n} thread: {:?}", std::thread::current());
+
+                while let Ok(mut test) = t_mutex.request_borrow().await {
+                    test.counter += 1;
+                    eprintln!("t{n}: counter: {}", test.counter);
+                    drop(test);
+                    Delay::new(Duration::from_millis(50)).await;
+                }
+            })
+        });
+        t.join().unwrap();
+    }
+
+    t1.join().unwrap();
+}
--- tests/borrow_dyn.rs
+++ tests/borrow_dyn.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright(c) 2024 Darek Stojaczyk
 
-use std::{any::Any, time::Duration};
+use std::{any::Any, sync::Arc, time::Duration};
 
 use futures::FutureExt;
 use futures_timer::Delay;
@@ -92,4 +92,175 @@ fn borrow_dyn_single_thread() {
             mutex.terminate().await;
         });
     });
+}
+
+#[test]
+fn borrow_dyn_multi_thread() {
+    let mutex = Arc::new(BorrowMutex::<16, dyn Any>::new());
+
+    let f1_mutex = mutex.clone();
+    let f1 = move || {
+        futures::executor::block_on(async move {
+            let mut test = TestObject { counter: 1 };
+            loop {
+                if test.counter >= 20 {
+                    break;
+                }
+                futures::select! {
+                    _ = Delay::new(Duration::from_millis(100)).fuse() => {
+                        if test.counter < 10 {
+                            test.counter += 1;
+                        }
+                        println!("t1: counter: {}", test.counter);
+                    }
+                    _ = f1_mutex.wait_to_lend().fuse() => {
+                        f1_mutex.lend(&mut test as &mut dyn Any).unwrap().await
+                    }
+                }
+            }
+        })
+    };
+
+    let f2_mutex = mutex.clone();
+    let f2 = move || {
+        futures::executor::block_on(async move {
+            let mut test = AnotherTestObject {
+                is_even: false,
+                another_counter: 1,
+            };
+            loop {
+                if test.another_counter >= 20 {
+                    break;
+                }
+                futures::select! {
+                    _ = Delay::new(Duration::from_millis(100)).fuse() => {
+                        if test.another_counter < 10 {
+                            test.another_counter += 1;
+                            test.is_even = !test.is_even;
+                        }
+                        println!("t2: another_counter: {}", test.another_counter);
+                        assert_eq!(test.is_even, test.another_counter % 2 == 0);
+                    }
+                    _ = f2_mutex.wait_to_lend().fuse() => {
+                        f2_mutex.lend(&mut test as &mut dyn Any).unwrap().await
+                    }
+                }
+            }
+        })
+    };
+
+    let tborrower_mutex = mutex.clone();
+    let tborrower = std::thread::spawn(|| {
+        futures::executor::block_on(async move {
+            while let Ok(mut any) = tborrower_mutex.request_borrow().await {
+                if let Some(test) = any.downcast_mut::<TestObject>() {
+                    test.counter += 1;
+                    println!("t3: counter: {}", test.counter);
+                } else if let Some(test) = any.downcast_mut::<AnotherTestObject>() {
+                    test.another_counter += 1;
+                    test.is_even = !test.is_even;
+                    println!("t3: another_counter: {}", test.another_counter);
+                    assert_eq!(test.is_even, test.another_counter % 2 == 0);
+                }
+                drop(any);
+                Delay::new(Duration::from_millis(200)).await;
+            }
+        })
+    });
+
+    std::thread::spawn(f1).join().unwrap();
+    std::thread::spawn(f2).join().unwrap();
+    futures::executor::block_on(async move {
+        mutex.terminate().await;
+    });
+    tborrower.join().unwrap();
+}
+
+#[test]
+fn borrow_dyn_multi_thread_multi_borrow() {
+    let mutex = Arc::new(BorrowMutex::<16, dyn Any>::new());
+
+    let f1_mutex = mutex.clone();
+    let f1 = move || {
+        futures::executor::block_on(async move {
+            let mut test = TestObject { counter: 1 };
+            loop {
+                if test.counter >= 20 {
+                    break;
+                }
+                futures::select! {
+                    _ = Delay::new(Duration::from_millis(100)).fuse() => {
+                        if test.counter < 10 {
+                            test.counter += 1;
+                        }
+                        println!("t1: counter: {}", test.counter);
+                    }
+                    _ = f1_mutex.wait_to_lend().fuse() => {
+                        f1_mutex.lend(&mut test as &mut dyn Any).unwrap().await
+                    }
+                }
+            }
+        })
+    };
+
+    let f2_mutex = mutex.clone();
+    let f2 = move || {
+        futures::executor::block_on(async move {
+            let mut test = AnotherTestObject {
+                is_even: false,
+                another_counter: 1,
+            };
+            loop {
+                if test.another_counter >= 20 {
+                    break;
+                }
+                futures::select! {
+                    _ = Delay::new(Duration::from_millis(100)).fuse() => {
+                        if test.another_counter < 10 {
+                            test.another_counter += 1;
+                            test.is_even = !test.is_even;
+                        }
+                        println!("t2: another_counter: {}", test.another_counter);
+                        assert_eq!(test.is_even, test.another_counter % 2 == 0);
+                    }
+                    _ = f2_mutex.wait_to_lend().fuse() => {
+                        f2_mutex.lend(&mut test as &mut dyn Any).unwrap().await
+                    }
+                }
+            }
+        })
+    };
+
+    let mut tborrowers = Vec::new();
+    for n in 0..3 {
+        let n = n + 3;
+        let tborrower_mutex = mutex.clone();
+        let tborrower = std::thread::spawn(move || {
+            futures::executor::block_on(async move {
+                while let Ok(mut any) = tborrower_mutex.request_borrow().await {
+                    if let Some(test) = any.downcast_mut::<TestObject>() {
+                        test.counter += 1;
+                        println!("t{n}: counter: {}", test.counter);
+                    } else if let Some(test) = any.downcast_mut::<AnotherTestObject>() {
+                        test.another_counter += 1;
+                        test.is_even = !test.is_even;
+                        println!("t{n}: another_counter: {}", test.another_counter);
+                        assert_eq!(test.is_even, test.another_counter % 2 == 0);
+                    }
+                    drop(any);
+                    Delay::new(Duration::from_millis(200)).await;
+                }
+            })
+        });
+        tborrowers.push(tborrower);
+    }
+
+    std::thread::spawn(f1).join().unwrap();
+    std::thread::spawn(f2).join().unwrap();
+    futures::executor::block_on(async move {
+        mutex.terminate().await;
+    });
+    for t in tborrowers {
+        t.join().unwrap();
+    }
 }
